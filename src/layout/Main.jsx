051970496757fcf545b8babb9a23840af5a3203a
ArturTurcan <105306163+ArturTurcan@users.noreply.github.com>
2022-07-21 14:26:54 +0300

    make api key protection

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -2,20 +2,20 @@ import React from "react";
 import { Movies } from "../components/Movies"
 import { Search } from "../components/Search"
 import { Preloader } from "../components/Preloader";
-
+const API_KEY = process.env.REACT_APP_API_KEY
 class Main extends React.Component {
     state = {
         movies: [],
         loading:true,  }
         componentDidMount() {
-            fetch("http://www.omdbapi.com/?apikey=88889831&s=matrix")
+            fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
                 .then((response) => response.json())
                 .then((data) => this.setState({movies: data.Search, loading: false}))
         }
 
        searchMovies = (str, type = "all") => {
         this.setState({loading: true})
-    fetch(`http://www.omdbapi.com/?apikey=88889831&s=${str}${type !== "all" ?`&type=${type}` : '' }`)
+    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${type !== "all" ?`&type=${type}` : '' }`)
 .then(response => response.json())
 .then((data) => this.setState({movies: data.Search, loading: false}))
 }
